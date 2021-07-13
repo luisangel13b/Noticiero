bb4e7c6497b701656026b7eb08910672c2edf491
luisangel13b <87395010+luisangel13b@users.noreply.github.com>
2021-07-13 15:18:28 -0600

    Add files via upload

---
+++ Html/index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"  crossorigin="anonymous">
+        
+        <title>Noticiero Sureño</title>
+	</head>
+    <body background="../img//t.jpg">
+
+                
+					
+				</div>
+            </div>
+       
+    <section>
+    <div class="row">
+	<div class="col-12">
+	<br>
+	<center><h2 class="border-bottom-h2 text-black font-weight-bold"> NOTICIAS EN NAYARIT </h2></center>
+    <br><p><center><img src="../img/thumbnailne.ashx.png" class="img-fluid"></center>
+					
+    </div>
+    <div class="col-12">
+    <center> <h2 class="border-bottom-h2 text-turquesa font-weight-bold"><br> Aqui tiene las noticias mas relevantes de la Semana</h2></center>
+    </div>
+    </div>
+    <br>
+    <div class="row">
+    <div class="img-box">
+                	
+    </div>
+												
+    <div class="col-sm-3">
+    <div class="thumb-wrapper">
+    <div class="img-box">
+    <img src="../img/P1.png" class="img-fluid">
+    </div>
+    <div class="thumb-content">
+												
+     <h4>Niegan amparo contra aseguramiento de bienes de Roberto Sandoval</h4>
+    <p>El exgobernador de Nayarit había impugnado el aseguramiento de un predio y una parcela. <a href="noticia1.php" >Seguir leyendo </i></a> </p>
+                									
+    </div>
+    </div>
+    </div>
+										
+    <div class="col-sm-3">
+    <div class="thumb-wrapper">
+    <div class="img-box">
+    <img src="../img/bala.jpg" class="img-fluid" alt="">
+     </div>
+												
+     <div class="thumb-content">
+     <h4>Enfrentamiento entre Guardia Nacional y hombres armados en Tepic deja un muerto</h4>
+    <p>El grupo armado disparó a los oficiales, lo que inició un enfrentamiento en el que un presunto delincuente murió y dos oficiales resultaron heridos.<a href="noticia.php" >Seguir Leyendo... </a></p> 
+                									
+    </div>
+    </div>
+    </div>
+    <div class="col-sm-3">
+    <div class="thumb-wrapper">
+    <div class="img-box">
+    <img src="../img/placas.jpeg"" class="img-fluid" alt="">
+     </div>
+     <div class="thumb-content">
+     <h4>Automóviles foráneos no podrán transitar por Jalisco, si no tienen permiso especial</h4>
+     <p>Esta tarde se dio a conocer que Jalisco aplicará medidas para los vehículos con placas que no pertenezcan al estado de mismos que deberán tramitar un permiso especial para poder circular en el estado el cual será durante plazos que no excedan los 20 días por semestre, a partir del próximo 1 de Agosto.<a href="noticia2.php" >Seguir leyendo </a></p>
+     </div>
+      </div>
+    </div>
+                
+	<div class="col-sm-3">
+     <div class="thumb-wrapper">
+      <div class="img-box">
+     <img src="../img/palo.jpeg" class="img-fluid" alt="">
+     </div>
+     <div class="thumb-content">
+     <h4>Dictan auto de formal prisión a Luis Cárdenas Palomino por tortura</h4>
+    <p>Un juez dictó auto de formal prisión en contra de Luis Cárdenas Palomino, exdirector de Seguridad Regional de la extinta Policía Federal, por su probable responsabilidad en el delito de tortura..<a href="noticia3.php" >Seguir leyendo </a></p>
+                									
+    </div>
+     </div>
+                		</div>
+				
+				</div>
+				
+				
+
+                        <p><center><img src="../img/coro.jpg" ></center>
+						
+                        <p><font size="4">
+
+El estado de Nayarit regresará oficialmente al semáforo epidemiológico amarillo el próximo lunes, como resultado de que sus autoridades han confirmado recientemente hasta 30 contagios de Covid-19 diarios, luego que su gobierno estatal llegó a certificar uno al día hace dos semanas.
+
+Este jueves, los integrantes de la Mesa Covid nayarita determinaron prohibir los jaripeos, fiestas patronales, bailes y reuniones masivas en general, en un intento por frenar el avance de la pandemia en el estado.
+
+Antonio Serrano, secretario general de Gobierno, explicó que asimismo se buscará “mantener la economía actuante”, de tal manera que “las unidades económicas o empresariales” operarán al 75 por ciento de sus capacidades. “Los horarios para restaurantes no serán recortados, sólo su capacidad de recibir comensales”, agregó.
+
+Destacó que si bien se incrementaron los contagios de coronavirus, no aumentaron las defunciones ni el número de personas hospitalizadas.
+
+Asimismo, Serrano Guzmán dio a conocer que si bien no se pidió a la Secretaría de Educación de Nayarit cancelar sus eventos de clausura de cursos, se le recomendó acoplarse a las medidas sanitarias ya conocidas, y “que si van a invitar a los dos papás del graduado, del niño o la niña, que solamente vaya uno y que sea en lugares abiertos”.<br></font></p>
+					
+        </section>
+       </html>
---
+++ Html/noticia.php
@@ -0,0 +1,60 @@
+
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <!--================================================
+        MANDAR LLAMAR CSS DE BOOSTRAP
+	    =================================================-->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+        <!--================================================
+        MANDAR LLAMAR CSS
+	    =================================================-->
+        <link rel="stylesheet" href="../css/main.css">
+        <!--================================================
+        MANDAR LLAMAR ICONOS BOOSTRAP
+	    =================================================-->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+        <title>Noticias del sur</title>
+    </head>
+    <body>
+<section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <br><br>
+						<h2 class="border-bottom-h2 text-turquesa font-weight-bold">Enfrentamiento entre Guardia Nacional y hombres armados en Tepic deja un muerto</h2>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 text-center">
+                        <span class="text-color-menu size-menu-3">El grupo armado disparó a los oficiales, lo que inició un enfrentamiento en el que un presunto delincuente murió y dos oficiales resultaron heridos.</span>
+						<hr></hr>
+					</div>
+                </div>
+				
+                <div class="row">
+                    <div class="col-6">
+                        
+                            <img src="../img/escena-del-crimen.jpg" class="img-fluid">
+                        
+						
+                    </div>
+					
+                    <div class="col-6">
+                       
+						<p>Un grupo de personas armadas atacó a elementos de la Guardia Nacional en Tepic, Nayarit, lo que provocó una persecución y un enfrentamiento en el que murió un presunto delincuente y dos oficiales resultaron heridos.</p>
+                        <p>De acuerdo con reportes oficiales de la Secretaría de Seguridad Pública del estado, a las 12:45 horas un grupo armado disparó a miembros de la Guardia Nacional en el poblado de San Cayetano, en la carretera que conduce a Pantanal.</p>
+						<p><font size="4">Tras el ataque, los guardias nacionales repelieron la agresión y los agresores intentaron huir, lo que inició una persecución y al llegar a la colonia 2 de agosto, los hombres armados se enfrentaron a los oficiales. </font></p>
+						</font>
+					</div>
+                    <div class="col-12">
+						<p><font size="4">En el lugar, un civil armado murió y dos más fueron detenidos; mientras que dos elementos de la Guardia Nacional resultaron heridos. El primer enfrentamiento se registró sobre la carretera que va de la comunidad de San Cayetano a El Pantanal, donde elementos de la Guardia Nacional que circulaban hacia el aeropuerto
+						Amado Nervo fueron atacados por un grupo de sujetos que se desplazaban en una camioneta en color blanco; en ese sitio dos elementos de la Guardia Nacional resultaron heridos, uno de ellos de gravedad, por lo que tuvo que ser trasladado de emergencia a un hospital, donde más tarde perdió la vida, el estado de salud del otro fue reportado como delicado.</font></p>
+						Por este hecho se inició un operativo de búsqueda de los responsables y más tarde otros elementos de la Guardia Nacional fueron atacados a la altura de la comunidad de El Dorado, por lo que inició otro tiroteo; en ese punto fue abatido uno de los agresores y se aseguró un vehículo. </font></p>
+
+                    <p><font size="4">Al operativo de búsqueda se unieron elementos de la Policía Estatal, de la Fiscalía del Estado y del Ejército Mexicano y minutos más tarde se registró un tercer enfrentamiento sobre la Avenida Tecnológico; tras el tiroteo dos civiles fueron arrestados y se aseguró una camioneta con armamento.   <a href="index.php" >Volver al Inicio</a>  </font></p>
+					</html>
